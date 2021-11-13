8884a4355a3973b059fc115e086365eb514e5e39
Kittanokas <kitai1012@gmail.com>
2021-11-13 12:13:50 +0900

    sessions/new.html.erb　の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :Kanji_last_name, presence: true
-  validates :Kanji_first_name, presence: true
-  validates :Kana_last_name, presence: true
-  validates :Kana_first_name, presence: true
-  validates :birthday, presence: true
+  validates :nickname,           presence: true
+  validates :Kanji_last_name,    presence: true
+  validates :Kanji_first_name,   presence: true
+  validates :Kana_last_name,     presence: true
+  validates :Kana_first_name,    presence: true
+  validates :birthday,           presence: true
   validates :encrypted_password, presence: true
 
   has_many :items
