6b70fb7e81887f906c5bb1f9a18cc803a05aea15
Kittanokas <kitai1012@gmail.com>
2021-11-14 11:40:33 +0900

    ActiveHashのデータベース設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
-   has_one_attached :image
-   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :name, :explanation, :price, :user, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  has_one_attached :image
+  belongs_to :prefecture
 end
