682b8843ecbfc43ff29ce1ab7bb223a769ca25f3
Kittanokas <kitai1012@gmail.com>
2021-11-16 13:45:35 +0900

    controllerへの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:item, :name, :image)
+    params.require(:item).permit(:nickname, :Kanji_last_name, :Kanji_first_name, :Kana_first_name, :Kana_last_name, :birthday, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :name, :explanation, :price, :user, presence: true
+  validates :name, :explanation, :category_id, :status_id, :delivery_id, :prefecture_id, :ship_day_id, :price, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
   has_one_attached :image
-  belongs_to :prefecture
+  belongs_to :prefecture, :status, :category, :delivery_fee, :ship_day
 end
