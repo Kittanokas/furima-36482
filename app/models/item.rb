657dd4511a79d52c232f0856fcc8fbf50a23f44b
Kittanokas <kitai1012@gmail.com>
2021-11-16 14:15:30 +0900

    modelの訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :name, :explanation, :category_id, :status_id, :delivery_id, :prefecture_id, :ship_day_id, :price, presence: true
+  validates :name, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :ship_day_id, :price, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
   has_one_attached :image
   belongs_to :prefecture, :status, :category, :delivery_fee, :ship_day
