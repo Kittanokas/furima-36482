b17687b389eaa63a2c35eb1ef957339411829790
Kittanokas <kitai1012@gmail.com>
2021-11-17 16:53:55 +0900

    itemモデルテストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :image, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } , presence: true
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+describe '商品の出品' do
+  context '商品の出品ができる場合' do
+    it '商品画像と説明文、価格を記述し出品できる' do
+      expect(@item).to be_valid
+    end
+  end
+  context '商品の出品ができない場合' do
+    it '商品画像が空では投稿できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it '商品名が空では投稿できない' do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+    it '商品説明が空では投稿できない' do
+      @item.explanation = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Explanation can't be blank")
+    end
+    it 'カテゴリーが空では投稿できない' do
+      @item.category_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+    it '商品状態が空では投稿できない' do
+      @item.status_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+    it '配送料の負担情報が空では投稿できない' do
+      @item.delivery_fee_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+    end
+    it '発送元地域情報が空では投稿できない' do
+      @item.prefecture_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it '発送までの日数情報が空では投稿できない' do
+      @item.ship_day_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship day can't be blank")
+    end
+    it '商品価格が空では投稿できない' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+  end
+end
 end
