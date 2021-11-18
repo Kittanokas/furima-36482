789dd9684c22bbab2855b6d76874643505caed3a
Kittanokas <kitai1012@gmail.com>
2021-11-18 14:50:54 +0900

    モデルの記述訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,9 @@ class Item < ApplicationRecord
   validates :ship_day_id, numericality: { other_than: 1, message: "can't be blank" } , presence: true
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format:{with: /\A[0-9]+\z/}, presence: true
   has_one_attached :image
-  belongs_to :prefecture, :status, :category, :delivery_fee, :ship_day
-  belongs_to :user
+  belongs_to :prefecture
+  belongs_to :status
+  belongs_to :category
+  belongs_to :delivery_fee
+  belongs_to :ship_day
 end
