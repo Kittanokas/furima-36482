1f2b7e1aa4746674daf4ce12a70e444a9e70175b
Kittanokas <kitai1012@gmail.com>
2021-11-19 18:54:02 +0900

    controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :move_to_index, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update]
+  #before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -34,10 +34,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
-    if @item.update(item_params)
+    item = Item.find(params[:id])
+    if item.update(item_params)
       redirect_to item_path
     else
       render :edit
@@ -51,13 +53,13 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    unless user_signed_in? == current_user
       redirect_to action: :index
     end
   end
 
   def contributor_confirmation
-    redirect_to root_path unless current_user == @item.user
+    redirect_to root_path unless @current_user.id != params[:id].to_i
   end
   
 end
