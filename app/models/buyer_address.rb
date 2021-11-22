18badd50d65c8eb288cb4add369e04af7f66bc70
Kittanokas <kitai1012@gmail.com>
2021-11-22 18:12:18 +0900

    model, controllerの修正

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,3 +1,8 @@
 class BuyerAddress < ApplicationRecord
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :postal_code, presence: true
+  validates :city, presence: true
+  validates :house_num, presence: true
+  validates :phone, presence: true
   belongs_to :order
 end
