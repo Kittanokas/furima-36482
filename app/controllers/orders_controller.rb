81ae553f7549ffe66e687f97933f022dff518560
Kittanokas <kitai1012@gmail.com>
2021-11-26 17:33:57 +0900

    payjpの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,18 +10,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:token, :postal_code, :city, :house_num, :phone).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:token, :postal_code, :city, :house_num, :phone, :building_name).merge(user_id: current_user.id, token: params[:token])
   end
 end
