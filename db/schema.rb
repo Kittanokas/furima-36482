732e589f3c363cb33af0f8c749c06f21f4368bb4
Kittanokas <kitai1012@gmail.com>
2021-12-02 11:15:01 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_040034) do
+ActiveRecord::Schema.define(version: 2021_11_29_035520) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
