1af81a3fbac55028f6a4e182db1d44cf3458e737
Kittanokas <kitai1012@gmail.com>
2021-12-02 15:58:57 +0900

    migrationファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2021_11_29_035520) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.integer "item_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
