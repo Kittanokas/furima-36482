b0774e41f93164596f6e95cb8e9237a74c771e5e
Kittanokas <kitai1012@gmail.com>
2022-01-14 17:33:04 +0900

    SNS認証修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: :google_oauth2
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   validates :nickname,           presence: true
   validates :Kanji_last_name,    presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
@@ -14,4 +14,19 @@ class User < ApplicationRecord
   has_many :orders
   has_many :comments
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
